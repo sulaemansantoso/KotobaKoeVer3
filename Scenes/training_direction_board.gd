e8b450d1fe9f3f81d13002d65c3197a35ebe622d
Sulaeman Santoso <sulaeman.santoso@gmail.com>
2024-05-30 16:14:07 +0700

    adding komentar penting

---
+++ Scenes/training_direction_board.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+enum Movement {
+	up,
+	down,
+	left,
+	right
+};
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _move_character()->void :
+	pass
+###
+ ### idenya adalah
+### ada satu room yang buat test semua
+### jadi ada beberapa test yang dilakukan dengan vr
+### ini dan sekaligus mengetest seberapa bagus each approach
+### kaya misalnya yang standar
+### atau yang nanti di test pake flash card
+### atau nanti juga di test menggunakan suara
+### atau juga est mnggunakan huruf
+###test menggunakan speech
+###
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
