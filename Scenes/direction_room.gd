502a1a6f8a290f8897310d8914159504180aed4e
Sulaeman Santoso <sulaeman.santoso@gmail.com>
2024-07-25 18:55:03 +0700

    adding third room functionality, bug on movement cause of vector 3 not moving on local

--- Scenes/direction_room.gd
+++ Scenes/direction_room.gd
@@ -8,6 +8,10 @@ var json_value
 var to_guess :Array = []
 var guess_right : int = 0
 
+func spawn_target() :
+	
+	pass
+
 func update_info(idx) :
 	$info_viweport.get_scene_instance().set_info(json_value[idx]["English"], json_value[idx]["Japanese"] + " " + json_value[idx]["Romaji"])
 	#json_value[idx]
@@ -18,21 +22,37 @@ func _ready():
 	var json_string =  FileAccess.get_file_as_string(json_file)
 	print(json_string)
 	json_value = JSON.parse_string(json_string)
-	#for board in $LearningBoard.get_children():
-		#if (board.has_method("pasang_resource")):
-			#board.pasang_resource(json_value)
-			#board.connect("ValueSelected",learning_tile_click)
-		#pass
+	var val2 : int = 0
+	for board in $LearningBoard.get_children():
+		if (board.has_method("pasang_resource")):
+			board.pasang_resource(json_value)
+			board.change_idx(val2)
+			val2 = val2 +1
+			board.connect("ValueSelected",learning_tile_click)
+		pass
 	
 		
-	#var val :int = 0
-	#for board in $TypingBoard.get_children() :
-		#if (board.has_method("pasang_resource")):
-			#board.pasang_resource(json_value)
-			#board.change_idx( val)
-			#val = (val +1)%12
-			#board.connect("ValueSelected", typing_tile_click)
+	var val :int = 0
+	for board in $TypingBoard.get_children() :
+		if (board.has_method("pasang_resource")):
+			board.pasang_resource(json_value)
+			board.change_idx( val)
+			val = (val +1)%6
+			board.connect("ValueSelected", typing_tile_click)
 	#randomize_duck()
 	#
 	pass
+	
+func typing_tile_click(idx) :
+	var audio_to_play = get_node("Audios/"+str(idx+1))
+	audio_to_play.play()
+	$TrainingDirectionBoard.move_character(idx)
+	pass
+
+func learning_tile_click(idx) :
+	var audio_to_play = get_node("Audios/"+str(idx+1))
+	audio_to_play.play()
+	update_info(idx)
+	pass
+	
 
--- Scenes/training_direction_board.gd
+++ Scenes/training_direction_board.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
+@export
+var target : PackedScene
+var target_instance : Area3D
+
 enum Movement {
 	up,
 	down,
@@ -7,11 +11,46 @@ enum Movement {
 	right
 };
 
+var speed : int = 32
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+
 	pass # Replace with function body.
 
-func _move_character()->void :
+func spawn_instance() :
+	var coord : Vector2
+	
+	coord = Vector2(randi_range(-16,17), randi_range(-18,16))
+	target_instance  = target.instantiate()
+	target_instance.position = Vector3(coord.x,-2.8, coord.y)
+	$GridMap.add_child(target_instance)
+	target_instance.connect("target_collected", target_collected_handle)
+	
+	# dari -16 16 
+	# ke 17 -18
+	pass
+	
+func target_collected_handle() :
+	$GridMap.remove_child(target_instance)
+	target_instance.queue_free()
+	$Timer.start()
+	pass
+
+func move_character(idx)->void :
+	var movement = Vector3(0,0,0)
+	if (idx == 0) :
+		movement = Vector3(-speed,0,0)
+	if (idx == 1) :
+		movement = Vector3(speed,0,0)
+	if (idx == 2) :
+		movement = Vector3(0,speed,0)
+	if (idx == 3) :
+		movement = Vector3(0,-speed,0)
+	
+	$GridMap/CharacterBody3D.velocity = movement
+	
+	$GridMap/CharacterBody3D.move_and_slide()
 	pass
 ###
  ### idenya adalah
@@ -29,3 +68,8 @@ func _move_character()->void :
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_timer_timeout():
+	spawn_instance()
+	pass # Replace with function body.
